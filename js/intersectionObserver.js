5a988963fb1ad12468196997a5e4d723e81eadf5
Magnus J <74900307+ThumbsUpGif@users.noreply.github.com>
2022-04-12 19:55:35 +0200

    commit- end of session

--- js/intersectionObserver.js
+++ js/intersectionObserver.js
@@ -1,22 +1,22 @@
 console.log('hei verden!!')
 
 // ///// Case 2 - Research ////
-const interviews = document.querySelectorAll('.animation-subheading')
+const quotes = document.querySelectorAll('.animation-quote')
 
-observerSubs = new IntersectionObserver((entries) => {
+observerQuotes = new IntersectionObserver((entries) => {
     console.log(entries);
 
     entries.forEach(entry => {
         if (entry.intersectionRatio > 0) {
             entry.target.style.animation =
-                `animation-interview 0.3s ${entry.target.dataset.delay} forwards ease`
-                observerSubs.unobserve(entry.target)
+                `animation-quote 0.7s ${entry.target.dataset.delay} forwards ease`
+                observerQuotes.unobserve(entry.target)
         } else {
             entry.target.style.animation = `none`;
         }
     })
 })
 
-interviews.forEach(interview => {
-    observerSubs.observe(interview)
+quotes.forEach(quote => {
+    observerQuotes.observe(quote)
 })
